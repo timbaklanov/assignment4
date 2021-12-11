52c85b9e2281bdc12c55415487135976be526b6f
Timur Baklanov <timbaklanov03@gmail.com>
2021-12-11 12:48:03 -0500

    Initial Commit

---
+++ finalproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalproject
+//
+//  Created by Тимур Бакланов on 11.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
